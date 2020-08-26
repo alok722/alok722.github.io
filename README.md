227395fbead6c866d5314c02394db388e09e19df
Alok Raj <alokr417@gmail.com>
2020-08-26 11:16:47 +0530

    🚀v1.0.0 initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+<h1 align="center">welcome to my portfolio 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+  <a href="https://github.com/open-devs/fastify-typescript-generator/graphs/commit-activity" target="_blank">
+    <img alt="Maintenance" src="https://img.shields.io/badge/Maintained-yes-blue.svg" />
+  </a>
+  <a href="https://github.com/open-devs/fastify-typescript-generator/blob/master/LICENSE" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/github/license/open-devs/fastify-typescript-generator" />
+  </a>
+</p>
+
+### 🏠 [homepage](https://alokraj.tech)
+
+## install
+
+```sh
+git clone https://github.com/alok722/portfolio.git
+```
+
+## usage
+
+```sh
+open index.html
+```
+
+## ❓ what is it
+
+a webpage to showcase my experience, education, skill-set, projects, certificates, achievements and recommendations.
+
+## 👤 author
+
+ **alok raj (alokr417@gmail.com)**
+
+* website: https://alokraj.tech/
+* linkedin: [@alok722](https://linkedin.com/in/alok722)
+
+## 🚀 future scope
+
+* adding dark mode toggle
+* adding blog section
+
+## show your support
+
+give a ⭐️ if you are using this template for your webpage.
+
+## 📝 license
+
+This project is [MIT](https://github.com/alok722/portfolio/blob/master/LICENSE) licensed.
+
+***
---
+++ js/main.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+
+    $('.card-toggle').on('click', function () {
+        if ($(this).find('svg').attr('data-icon') == 'chevron-down') {
+            $(this).find('svg').attr('data-icon', 'chevron-up');
+        } else {
+            $(this).find('svg').attr('data-icon', 'chevron-down');
+        };
+    });
+
+    let $btns = $('.project-area .button-group button');
+
+    $btns.click(function (e) {
+
+        $('.project-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.project-area .grid').isotope({
+            filter: selector
+        });
+
+        return false;
+    })
+
+    $('.project-area .button-group #btn1').trigger('click');
+
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+    // Owl-carousel
+
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            560: {
+                items: 2
+            }
+        }
+    })
+
+});
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scroll-btn").style.display = "block";
+    }
+    else {
+        document.getElementById("scroll-btn").style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox and Opera
+}
